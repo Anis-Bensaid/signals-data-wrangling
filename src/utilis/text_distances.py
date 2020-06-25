2bda2aabd50b55c61b076188f4df6c875fb2d688
Anis BEN SAID <abensaid@mit.edu>
2020-06-25 09:37:21 -0400

    Minor bug fixes.

--- src/utilis/text_distances.py
+++ src/utilis/text_distances.py
@@ -15,13 +15,13 @@ def brands_custom_distance(row):
     return np.average([fuzzi, jaro], weights=[0.4, 0.6])
 
 
-def names_custom_distance(row):
+def item_description_custom_distance(row):
     """
     Calculates a distance score between two sentences. In this case elc_brand and brand. The score is between 0 and 1, 1 being a good match.
     """
     # jaccard = textdistance.jaccard(str(row['brand']).lower().replace('.','').replace('&','and'), str(row['elc_brand']).lower().replace('.','').replace('é','e'))
-    jaro = textdistance.jaro_winkler(str(row['product']).lower(), str(row['item_description']).lower())
-    fuzzi = fuzz.partial_ratio(str(row['product']).lower(), str(row['item_description']).lower()) / 100
+    jaro = textdistance.jaro_winkler(str(row['Product']).lower(), str(row['Item_Description']).lower())
+    fuzzi = fuzz.partial_ratio(str(row['Product']).lower(), str(row['Item_Description']).lower()) / 100
     return np.average([fuzzi, jaro], weights=[0.45, 0.55])
 
 
@@ -29,8 +29,8 @@ def subcategory_custom_distance(row):
     """
     Calculates a distance score between two sentences. In this case elc_brand and brand. The score is between 0 and 1, 1 being a good match.
     """
-    jaro = textdistance.jaro_winkler(str(row['sub_category']).lower(),
-                                     str(row['item_description']).lower().replace('.', '').replace('&', 'and'))
-    fuzzi = fuzz.partial_ratio(str(row['item_description']).lower().replace('.', '').replace('&', 'and'),
-                               str(row['sub_category'])) / 100
+    jaro = textdistance.jaro_winkler(str(row['Sub_Category']).lower(),
+                                     str(row['Item_Description']).lower().replace('.', '').replace('&', 'and'))
+    fuzzi = fuzz.partial_ratio(str(row['Item_Description']).lower().replace('.', '').replace('&', 'and'),
+                               str(row['Sub_Category'])) / 100
     return np.average([fuzzi, jaro], weights=[0.5, 0.5])
